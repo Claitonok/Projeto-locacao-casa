0b84fa232d069536a4bc6b2508e821dbb77ec09c
Claitonok <claiton_santos1@hotmail.com>
2026-01-27 17:26:58 -0300

    27/01/2026

--- projeto/pastajs/formataFoto.js
+++ projeto/pastajs/formataFoto.js
@@ -5,12 +5,18 @@ const profilePic = document.getElementById('foto');
 
 profilePic.addEventListener('change', function(event) {
     const file = event.target.files[0];
-    if (file) {
+
+    var extensoesPermitidas = /(.jpg|.jpeg|.png|.gif)$/i;
+
+    if (extensoesPermitidas.exec(file.name)) {
         const reader = new FileReader();
         reader.onload = function(e) {
             const imgElement = document.getElementById('Foto');
             imgElement.src = e.target.result;
         };
         reader.readAsDataURL(file);
+    } else {
+        alert('Por favor, selecione um arquivo de imagem válido (JPG, JPEG, PNG, GIF).');
+        profilePic.value = '';
     }
 });
